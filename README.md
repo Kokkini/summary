d68a293638d0d3033e89b374d9fc142bef0ccd3c
Kokkini <trannhatquang1104@gmail.com>
2017-06-24 14:17:14 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+On branch master
+
+Initial commit
+
+Changes to be committed:
+  (use "git rm --cached <file>..." to unstage)
+
+	new file:   .classpath
+	new file:   .project
+	new file:   .settings/org.eclipse.jdt.core.prefs
+	new file:   bin/application/Main.class
+	new file:   bin/application/Main2.class
+	new file:   bin/application/Main3.class
+	new file:   bin/application/Test.class
+	new file:   src/application/Main.java
+	new file:   src/application/Main2.java
+	new file:   src/application/Main3.java
+	new file:   src/application/Test.java
+
+Untracked files:
+  (use "git add <file>..." to include in what will be committed)
+
+	README.md
+	bin/application/commons-codec-1.9.jar
+	bin/application/commons-logging-1.2.jar
+	bin/application/fluent-hc-4.5.3.jar
+	bin/application/httpclient-4.5.3.jar
+	bin/application/httpclient-cache-4.5.3.jar
+	bin/application/httpclient-win-4.5.3.jar
+	bin/application/httpcore-4.4.6.jar
+	bin/application/httpmime-4.5.3.jar
+	bin/application/jna-4.1.0.jar
+	bin/application/jna-platform-4.1.0.jar
+	src/application/commons-codec-1.9.jar
+	src/application/commons-logging-1.2.jar
+	src/application/fluent-hc-4.5.3.jar
+	src/application/httpclient-4.5.3.jar
+	src/application/httpclient-cache-4.5.3.jar
+	src/application/httpclient-win-4.5.3.jar
+	src/application/httpcore-4.4.6.jar
+	src/application/httpmime-4.5.3.jar
+	src/application/jna-4.1.0.jar
+	src/application/jna-platform-4.1.0.jar
+
---
+++ src/application/Main.java
@@ -0,0 +1,53 @@
+// // This sample uses the Apache HTTP client from HTTP Components (http://hc.apache.org/httpcomponents-client-ga/)
+package application;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Scanner;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        HttpClient httpClient = new DefaultHttpClient();
+        String URL = askForURL();
+        
+
+        try
+        {
+        	String link = "http://api.smmry.com/&SM_API_KEY=A34CD89F91&SM_LENGTH=3&SM_WITH_BREAK&SM_URL=";
+        	link = link + URL;
+            URIBuilder uriBuilder = new URIBuilder(link);
+
+            URI uri = uriBuilder.build();
+            HttpPost request = new HttpPost(uri);
+
+            HttpResponse response = httpClient.execute(request);
+            HttpEntity entity = response.getEntity();
+
+            if (entity != null)
+            {
+                System.out.println(EntityUtils.toString(entity));
+            }
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
+    public static String askForURL(){
+    	System.out.println("Paste URL here: ");
+    	Scanner reader = new Scanner(System.in);
+    	String s = reader.next();
+    	reader.close();
+    	return s;
+    	
+    }
+}
